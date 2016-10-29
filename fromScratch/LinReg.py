02c258f3262792fca3229e9c04503a27ad5e0194
abhai <abhai.dilip@gmail.com>
2016-10-30 02:00:11 +0530

    Linear Regression from scratch

---
+++ fromScratch/LinReg.py
@@ -0,0 +1,47 @@
+import numpy as np
+np.set_printoptions(suppress=True)
+
+#Import data
+train_features=np.loadtxt(fname="../datasets/linearReg.csv",delimiter=',',usecols=(0,1,2,3),dtype='float64')
+train_labels=np.loadtxt(fname="../datasets/linearReg.csv",delimiter=',',usecols=(4,),dtype='float64')
+train_labels = train_labels.reshape(-1, 1)
+
+
+class LinearRegression:
+    
+    def __init__(self):
+        self.ALPHA = 0.5
+
+    def scale(self, data):
+        denominator = np.amax(data, axis=0)
+        # print (data/denominator)
+        return (data/denominator), denominator
+    
+    def train(self, X, y):
+        self.N_ITEMS = X.shape[0]
+        self.weights = abs(np.random.randn(X.shape[1]).reshape(1,-1))
+
+        X, self.xdenom = self.scale(X)
+        y, self.ydenom = self.scale(y)
+        #Gradient descent
+        for i in range(100):
+            difference = self._predict(X)-y
+            cost = np.sum(np.square(difference))/(2*self.N_ITEMS)   #Computing cost function
+            self.weights = self.weights - (self.ALPHA/self.N_ITEMS)*(np.sum((difference*X), axis=0)) #Performing gradient descent
+            #print self.weights #Uncomment to see weights improving
+
+    def _predict(self, X):
+        return np.matmul(X, self.weights.reshape(-1,1))
+
+    def predict(self, X):
+        scaled = X/self.xdenom
+        return self._predict(scaled)*self.ydenom
+
+test = np.array([[1,1,1,1]],dtype="float64")
+
+model = LinearRegression()
+model.train(train_features,train_labels)
+
+ans=model.predict(test)
+
+print ans
