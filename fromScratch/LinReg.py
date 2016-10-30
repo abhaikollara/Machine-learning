abbdfccf89b2b7f68546a8a783302d95a006468a
abhai <abhai.dilip@gmail.com>
2016-10-30 15:08:26 +0530

    Added bias term

--- fromScratch/LinReg.py
+++ fromScratch/LinReg.py
@@ -3,7 +3,7 @@ np.set_printoptions(suppress=True)
 
 #Import data
 train_features=np.loadtxt(fname="../datasets/linearReg.csv",delimiter=',',usecols=(0,1,2,3),dtype='float64')
-train_labels=np.loadtxt(fname="../datasets/linearReg.csv",delimiter=',',usecols=(4,),dtype='float64')
+train_labels=np.loadtxt(fname="../datasets/linearReg.csv",delimiter=',',usecols=(4,),dtype='float32')
 train_labels = train_labels.reshape(-1, 1)
 
 
@@ -19,16 +19,17 @@ class LinearRegression:
     
     def train(self, X, y):
         self.N_ITEMS = X.shape[0]
+        X = np.concatenate((np.ones(self.N_ITEMS).reshape(-1,1),X),axis=1)
         self.weights = abs(np.random.randn(X.shape[1]).reshape(1,-1))
-
         X, self.xdenom = self.scale(X)
         y, self.ydenom = self.scale(y)
         #Gradient descent
         for i in range(100):
             difference = self._predict(X)-y
             cost = np.sum(np.square(difference))/(2*self.N_ITEMS)   #Computing cost function
-            self.weights = self.weights - (self.ALPHA/self.N_ITEMS)*(np.sum((difference*X), axis=0)) #Performing gradient descent
-            #print self.weights #Uncomment to see weights improving
+            self.weights[:,1:] = self.weights[:,1:] - (self.ALPHA/self.N_ITEMS)*(np.sum((difference*X[:,1:]), axis=0)) #Performing gradient descent
+            self.weights[:,0] = self.weights[:,0] - (self.ALPHA/self.N_ITEMS)*(np.sum((difference), axis=0)) #Performing gradient descent
+            print self.weights #Uncomment to see weights improving
 
     def _predict(self, X):
         return np.matmul(X, self.weights.reshape(-1,1))
@@ -37,7 +38,7 @@ class LinearRegression:
         scaled = X/self.xdenom
         return self._predict(scaled)*self.ydenom
 
-test = np.array([[1,1,1,1]],dtype="float64")
+test = np.array([[1,2,2,2,2]],dtype="float32")
 
 model = LinearRegression()
 model.train(train_features,train_labels)
